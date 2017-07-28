7c832402a590a55790aa223186e7486c3ee623dd
Ryan <r.walter215@gmail.com>
2017-07-28 14:05:04 -0500

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//CommentList.js
+import React, {Component} from 'react';
+import CommentBox from './containers/CommentBox';
+import style from './style';
+
+class CommentList extends Component {
+  render() {
+    return (
+      <CommentBox />
+    )
+  }
+}
+export default CommentList;
---
+++ src/containers/CommentBox.js
@@ -0,0 +1,20 @@
+//CommentBox.js
+import React, {Component} from 'react';
+import DATA from './data';
+import {Button} from 'semantic-ui-react';
+import style from './style';
+
+class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+  render() {
+    return (
+        <Button primary>Howdy</Button>
+    )
+  }
+}
+export default CommentBox;
