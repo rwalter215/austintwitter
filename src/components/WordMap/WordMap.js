98d105d86093f7de270c19c035c63effa1b0ea50
Ryan <r.walter215@gmail.com>
2017-07-30 21:42:56 -0500

    further tweaking ofr wordcloud, added models to get actual data

--- src/components/WordMap/WordMap.js
+++ src/components/WordMap/WordMap.js
@@ -19,7 +19,7 @@ class WordMap extends Component {
   componentDidMount() {
     const canvas = ReactDOM.findDOMNode(this.refs.canvas)
     const states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
-    let test = []
+    let test = [['ATX', 12]]
     states.map((word) => {
       test.push([word, Math.floor(Math.random() * 5) + 1  ])
     })
@@ -28,11 +28,15 @@ class WordMap extends Component {
     const options = {
       list: test,
       shape: "circle",
-      color:"green",
-      fontFamily: "Finger Paint, cursive, sans-serif",
-      weightFactor: 5,
+      color:(word, color) => {
+        return (word === 'ATX') ? '#ba5c12' : '#ffb86f';
+      },
+      fontFamily: "Times, serif",
+      weightFactor: 10,
+      rotateRatio: 0.5,
+      rotationSteps: 2,
       wait: 0,
-      backgroundColor:"black"
+      backgroundColor:"#261132"
     }
 
     WordCloud(canvas, options);
